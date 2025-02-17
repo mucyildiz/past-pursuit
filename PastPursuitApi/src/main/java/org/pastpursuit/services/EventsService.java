4e1bd131b1ce27085611affdea6bee7dadec45fa
Mucahit Yildiz <alperenyildiz21@gmail.com>
2025-02-16 20:07:36 -0500

    Make more efficient

--- PastPursuitApi/src/main/java/org/pastpursuit/services/EventsService.java
+++ PastPursuitApi/src/main/java/org/pastpursuit/services/EventsService.java
@@ -12,44 +12,45 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 @Path("past-pursuit/events")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class EventsService {
   private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-  private final Logger LOG = LoggerFactory.getLogger(EventsService.class);
-
+  private static final Logger LOG = LoggerFactory.getLogger(EventsService.class);
+  private final Map<Integer, List<HistoricalEvent>> eventsByYear;
+  private final List<Integer> years;
+  private final Random random = new Random();
 
   public EventsService() {
+    InputStream inputStream = getClass().getClassLoader()
+      .getResourceAsStream("formatted-events.json");
+    if (inputStream == null) {
+      throw new IllegalStateException("formatted-events.json not found in the classpath");
+    }
+    try {
+      eventsByYear = OBJECT_MAPPER.readValue(inputStream, new TypeReference<>() {
+      });
+    } catch (IOException e) {
+      LOG.error("Error reading events from file", e);
+      throw new IllegalStateException("Error reading events.");
+    }
+    years = new ArrayList<>(eventsByYear.keySet());
   }
 
   public HistoricalEvent getRandomEvent() {
-    List<HistoricalEvent> historicalEvents = getEvents();
-    LOG.info("Returned events: {}", historicalEvents);
-    return historicalEvents.get(new Random().nextInt(historicalEvents.size()));
-  }
+    Integer randomYear = years.get(random.nextInt(years.size()));
+    List<HistoricalEvent> historicalEvents = eventsByYear.get(randomYear);
+    if (historicalEvents == null) {
+      LOG.error("No events found for year {}", randomYear);
+      throw new IllegalStateException("No events found");
+    }
+    return historicalEvents.get(random.nextInt(historicalEvents.size()));
 
-  private List<HistoricalEvent> getEvents() {
-    InputStream inputStream = getClass().getClassLoader().getResourceAsStream("formatted-events.json");
-    if (inputStream == null) {
-      LOG.error("formatted-events.json not found in the classpath");
-      return Collections.emptyList();
-    }
-    try {
-      Map<Integer, List<HistoricalEvent>> eventsByYear = OBJECT_MAPPER.readValue(inputStream, new TypeReference<>() {
-      });
-      List<Integer> years = new ArrayList<>(eventsByYear.keySet());
-      List<HistoricalEvent> historicalEvents = eventsByYear.get(years.get(new Random().nextInt(years.size())));
-      if (historicalEvents == null) {
-        LOG.error("No events found");
-        return Collections.emptyList();
-      }
-      return historicalEvents;
-    } catch (IOException e) {
-      LOG.error("Error reading events from file", e);
-      return Collections.emptyList();
-    }
   }
 }
